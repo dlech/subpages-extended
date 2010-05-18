2fc3f3f9ecaf939f6993827c2546c9903706e035
mattsay <mattsay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-18 17:15:22 +0000

    First release files.
    git-svn-id: https://plugins.svn.wordpress.org/subpages-extended/trunk@242820 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Subpages Extended ===
+Contributors: Matt Say
+Donate link: http://shailan.com/donate
+Tags: subpages, widget, shortcode, navigation, menu, auto
+Requires at least: 2.5
+Tested up to: 2.9.2
+Stable tag: 1.0
+
+A multi widget to list subpages of a page. It also comes with a [[subpages]] shortcode. You can find more widgets, plugins and themes at shailan.com (http://shailan.com).
+
+== Description ==
+
+This widget displays subpages of a page easily. Though it’s main power is the [subpages] shortcode. Using this shortcode on a page you can create subpage indexes. You can view live demo on my wordpress page. It automatically generates subpage indexes. You can also list subpages of another page using the childof attribute of shortcode. See the examples below:
+
+Here are subpages of my wordpress page with a depth level of 1:
+`[[subpages depth="1" childof="286"]]`
+
+Outputs:
+
+* Plugins
+* Themes
+
+If the page doesn’t have any subpages it will display the following error for you to fix it:
+`[[subpages depth="1" childof="257"]]`
+
+Outputs: 
+
+"Services" doesn't have any sub pages.
+
+== Installation ==
+
+1. Download the widget and upload it to your server through `WP Admin -> Plugins -> Add New -> Upload`
+1. After the upload is complete activate the plugin.
+1. Go to Appearance -> Widgets page, drag and drop the widget to your sidebar.
+1. Fill in the blanks as needed, and done!
+
+== Frequently Asked Questions ==
+
+= Any questions? =
+
+You can ask your questions [here](http://shailan.com/contact)
+
+== Screenshots ==
+
+1. A snapshot of the widget form.
+
+== Changelog ==
+
+= 1.0 =
+* First release.
---
+++ subpages-widget.php
@@ -0,0 +1,134 @@
+<?php 
+/*
+Plugin Name: Shailan Subpages Widget
+Plugin URI: http://shailan.com/wordpress/plugins/subpages-widget
+Description: A multi widget to list subpages of a page. It also comes with a [subpages] shortcode. You can read <a href="http://shailan.com/wordpress/plugins/subpages-widget#usage">how to use subpages</a> . You can find more widgets, plugins and themes at <a href="http://shailan.com">shailan.com</a>.
+Version: 0.2
+Author: Matt Say
+Author URI: http://shailan.com
+*/
+
+define('SHAILAN_SP_VERSION','0.2');
+define('SHAILAN_SP_TITLE', 'Subpages List');
+
+/**
+ * Shailan Subpages Widget Class
+ */
+class shailan_SubpagesWidget extends WP_Widget {
+    /** constructor */
+    function shailan_SubpagesWidget() {
+		$widget_ops = array('classname' => 'shailan_SubpagesWidget', 'description' => __( 'Subpages list' ) );
+		$this->WP_Widget('shailan-subpages-widget', __('Subpages'), $widget_ops);
+		$this->alt_option_name = 'widget_shailan_subpages';
+		
+		if ( is_active_widget(false, false, $this->id_base) )
+			add_action( 'wp_head', array(&$this, 'styles') );		
+    }
+	
+    /** @see WP_Widget::widget */
+    function widget($args, $instance) {		
+		global $post;
+	
+        extract( $args );
+        $title = apply_filters('widget_title', $instance['title']);
+		$exclude = $instance['exclude'];
+		$depth = $instance['depth'];
+		
+		if(is_page()){				
+
+			//$parent = $childof;
+			//if($parent==''){ $parent = $post->ID; }
+			
+			$parent = $post->ID;
+			$children=wp_list_pages( 'echo=0&child_of=' . $parent . '&title_li=' );
+		
+			if ($children) {		
+			?>
+				  <?php echo $before_widget; ?>
+					<?php if ( $title )
+							echo $before_title . $title . $after_title;
+					?>
+
+				<div id="shailan-subpages-<?php echo $this->number; ?>">
+					<ul class="subpages">
+						<?php wp_list_pages('sort_column=menu_order&depth='.$depth.'&title_li=&child_of='.$post->ID.'&exclude='.$exclude); ?>
+					</ul>
+				</div> 			
+				
+				  <?php echo $after_widget; ?>
+			<?php
+			} else {
+				echo "\n\t<!-- SUBPAGES : This page doesn't have any subpages. -->";
+			};
+		}
+		
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {				
+        return $new_instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {				
+        $title = esc_attr($instance['title']);
+		$exclude = $instance['exclude'];
+		$depth = $instance['depth'];
+		
+        ?>		
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title (won\'t be shown):'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+			
+		<p><label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e('Exclude:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('exclude'); ?>" name="<?php echo $this->get_field_name('exclude'); ?>" type="text" value="<?php echo $exclude; ?>" /></label><br /> 
+		<small>Page IDs, separated by commas.</small></p>
+		
+		<p><label for="<?php echo $this->get_field_id('depth'); ?>"><?php _e('Depth:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('depth'); ?>" name="<?php echo $this->get_field_name('depth'); ?>" type="text" value="<?php echo $depth; ?>" /></label><br /> 
+		<small>Depth of menu.</small></p>
+			
+<div class="widget-control-actions alignright">
+<p><small><a href="http://shailan.com/wordpress/plugins/subpages-widget">Visit plugin site</a></small></p>
+</div>
+			
+        <?php 
+	}
+	
+	function styles($instance){
+		// additional styles will be printed here.
+	}
+
+} // class shailan_SubpagesWidget
+
+// register widget
+add_action('widgets_init', create_function('', 'return register_widget("shailan_SubpagesWidget");'));
+
+// [bartag foo="foo-value"]
+function shailan_subpages_shortcode($atts) {
+	global $post;
+
+	extract(shortcode_atts(array(
+		'depth' => '3',
+		'exclude' => '',
+		'childof' => '',
+		'exceptme' => false
+	), $atts));
+
+	$parent = $childof;
+	if($parent==''){ $parent = $post->ID; }
+	
+	if($exceptme) { $exclude .= ','.$post->ID; }
+
+	$children=wp_list_pages( 'echo=0&child_of=' . $parent . '&title_li=' );
+	
+	if ($children) {
+		$subpages = '<div id="shailan-subpages-<?php echo $this->number; ?>"><ul class="subpages">';
+		$subpages .= wp_list_pages('echo=0&sort_column=menu_order&depth='.$depth.'&title_li=&child_of='.$parent.'&exclude='.$exclude);
+		$subpages .= '</ul></div>';
+	} else {
+		$subpages = '"' . get_the_title($parent) . '" doesn\'t have any sub pages.';
+	}
+		
+	return $subpages;
+}
+add_shortcode('subpages', 'shailan_subpages_shortcode');
+
+
+
