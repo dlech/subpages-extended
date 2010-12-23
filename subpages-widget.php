61a0c2b88b663d740232e1a613b5090621480a6f
mattsay <mattsay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-23 18:13:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/subpages-extended/trunk@325719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- subpages-widget.php
+++ subpages-widget.php
@@ -17,8 +17,8 @@ define('SHAILAN_SP_TITLE', 'Subpages List');
 class shailan_SubpagesWidget extends WP_Widget {
     /** constructor */
     function shailan_SubpagesWidget() {
-		$widget_ops = array('classname' => 'shailan_SubpagesWidget', 'description' => __( 'Subpages list' ) );
-		$this->WP_Widget('shailan-subpages-widget', __('Subpages Extended'), $widget_ops);
+		$widget_ops = array('classname' => 'shailan_SubpagesWidget', 'description' => __( 'Subpages list', 'subpages-extended' ) );
+		$this->WP_Widget('shailan-subpages-widget', __('Subpages Extended', 'subpages-extended'), $widget_ops);
 		$this->alt_option_name = 'widget_shailan_subpages';
 		
 		if ( is_active_widget(false, false, $this->id_base) )
@@ -99,6 +99,107 @@ class shailan_SubpagesWidget extends WP_Widget {
 
 // register widget
 add_action('widgets_init', create_function('', 'return register_widget("shailan_SubpagesWidget");'));
+
+function subpages_widget_adminMenu(){
+
+	if(is_admin()){ 
+		wp_enqueue_script( 'jquery' );
+		//wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
+		wp_enqueue_script( 'tweetable', WP_PLUGIN_URL . '/subpages-extended/js/jquery.tweetable.js', 'jquery' );
+		wp_enqueue_style( 'tweetable', WP_PLUGIN_URL . '/subpages-extended/css/tweetable.css' );
+	};
+
+
+	if ( @$_GET['page'] == 'subpages-extended' ) {
+		if ( @$_REQUEST['action'] && 'save' == $_REQUEST['action'] ) {
+			//update_option( 'shailan_adsense_id', $_REQUEST['shailan_adsense_id'] );
+		}
+	}
+
+	if (function_exists('add_options_page')) {
+			$page = add_options_page(__('Subpages Options', 'subpages-extended') , __('Adsense Widget', 'subpages-extended'), 'edit_themes', 'subpages-extended', 'subpages_widget_options_page');
+	}
+}
+// add admin menu
+add_action('admin_menu', 'subpages_widget_adminMenu');
+
+function subpages_widget_options_page(){
+
+	$title = "Subpages Extended Options";
+	?>
+
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2><?php echo esc_html( $title ); ?></h2>
+
+<div class="nav"><small><a href="http://shailan.com/wordpress/plugins/subpages-widget">Plugin page</a> | <a href="http://shailan.com/wordpress/plugins/subpages-widget/help">Usage</a> | <a href="http://shailan.com/wordpress/plugins/subpages-widget/shortcode">Shortcode</a> | <a href="http://shailan.com/donate">Donate</a> | <a href="http://shailan.com/wordpress">Get more widgets..</a></small></div>
+
+<?php if ( isset($_GET['message']) && isset($messages[$_GET['message']]) ) { ?>
+<div id="message" class="updated"><p><?php echo $messages[$_GET['message']]; ?></p></div>
+<?php } ?>
+<?php if ( isset($_GET['error']) && isset($errors[$_GET['error']]) ) { ?>
+<div id="message" class="error"><p><?php echo $errors[$_GET['error']]; ?></p></div>
+<?php } ?>
+
+<form id="frmShailanDm" name="frmShailanDm" method="post" action="">
+
+<table class="form-table"> 
+<tr valign="top"> 
+<th scope="row"><label for="shailan_adsense_id"><?php _e('Adsense ID:'); ?></label></th> 
+<td><input name="shailan_adsense_id" id="shailan_adsense_id" type="text" value="<?php if ( get_option( 'shailan_adsense_id' ) != "") { echo stripslashes(get_option( 'shailan_adsense_id' )); } else { echo ''; } ?>" size="44" />
+<span class="description">Your unique Adsense ID.</span></td> 
+</tr> 
+</table>
+
+<input type="hidden" name="action" value="save" />
+
+<p class="submit"> 
+<input type="submit" name="Submit" class="button-primary" value="Save Changes" /> 
+</p> 
+ 
+</form>
+
+<table><tr><td width="400" valign="top">
+<div id="shailancom" style="background:#ededed; border-top:10px solid #ff9966; padding:15px;">
+<h3>Latest headlines from Shailan.com</h3>
+		<?php	
+			
+			$rss_options = array(
+				'link' => 'http://shailan.com',
+				'url' => 'http://feeds.feedburner.com/shailan',
+				'title' => 'Shailan.com',
+				'items' => 5,
+				'show_summary' => 0,
+				'show_author' => 0,
+				'show_date' => 0,
+				'before' => 'text'
+			);
+
+			wp_widget_rss_output( $rss_options ); ?>
+</div>
+</td><td width="400" valign="top">
+<div id="twit" style="background:#ededed; border-top:10px solid #68d8fd; padding:15px;">
+<h3>My latest tweets</h3>
+<div id="tweets">
+</div>
+</div>
+</td></tr>
+</table>
+
+<script type="text/javascript"> 
+jQuery(document).ready(function($) {
+	$('#tweets').tweetable({username: 'mattsay', time: false, limit: 2, replies: false});
+});
+</script> 
+
+<p>
+<small><a href="http://shailan.com/wordpress/plugins/subpages-widget">Subpages Extended</a> by <a href="http://shailan.com">shailan</a>.</small>
+</p>
+
+</div>
+
+<?php
+}
 
 // [bartag foo="foo-value"]
 function shailan_subpages_shortcode($atts) {
